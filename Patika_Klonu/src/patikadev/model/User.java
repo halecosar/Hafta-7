ad2e055868ee18b2fa0af309d6678723db5c5888
halecosar <halealtunakar@hotmail.com>
2023-10-20 21:02:08 +0300

    updates

--- Patika_Klonu/src/patikadev/model/User.java
+++ Patika_Klonu/src/patikadev/model/User.java
@@ -9,6 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.concurrent.RecursiveTask;
 
 public class User {
     private int id;
@@ -109,11 +110,11 @@ public class User {
             pr.setString(3, pass);
             pr.setString(4, type);
 
-            int response =pr.executeUpdate();
-            if ( response==-1){
+            int response = pr.executeUpdate();
+            if (response == -1) {
                 Helper.showMessage("error");
             }
-            return response!=-1;
+            return response != -1;
             // database'e gönderelim;
 
 
@@ -144,6 +145,78 @@ public class User {
         }
 
         return obj;
+    }
+
+    public static boolean delete(int id) {
+        String query = "DELETE FROM user WHERE id = ? ";
+
+        try {
+            PreparedStatement pr = DBconnector.getInstance().prepareStatement(query);
+            pr.setInt(1, id);
+            return pr.executeUpdate() != -1;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return true;
+    }
+
+    public static boolean update(int id, String name, String uname, String pass, String type) {
+        String query = "UPDATE user SET name=?,uname=?,pass=?,type=?,id=? ";
+        User findUser = User.getFetch(uname);
+        if (findUser != null && findUser.getId()!=id){
+            Helper.showMessage("Bu kullanıcı adı daha önceden eklenmiş. Lütfen farklı bir kullanıcı adı giriniz.");
+
+            return false;
+        }
+        try {
+            PreparedStatement pr = DBconnector.getInstance().prepareStatement(query);
+            pr.setString(1, name);
+            pr.setString(2, uname);
+            pr.setString(3, pass);
+            pr.setString(4, type);
+            pr.setInt(5, id);
+            return pr.executeUpdate() != -1;
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return true;
+    }
+    public static ArrayList<User> SearchuserList(String query) {
+        ArrayList<User> userList = new ArrayList<>();
+
+        User obj;
+        try {
+            Statement st = DBconnector.getInstance().createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                obj = new User();
+                obj.setId(rs.getInt("id"));
+                obj.setName(rs.getString("name"));
+                obj.setUname(rs.getString("uname"));
+                obj.setPassword(rs.getString("pass"));
+                obj.setType(rs.getString("type"));
+                userList.add(obj);
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return userList;
+    }
+
+        public static String searchQuery(String name, String uname, String type){
+            String query = "SELECT * FROM user WHERE uname LIKE '½{{uname}}½' AND name LIKE '½{{name}}½'";
+            query= query.replace("{{uname}}",uname);
+            query= query.replace("{{name}}",name);
+            if (!type.isEmpty()){
+                query += "AND TYPE= '{{type}}'";
+                query = query.replace("{{type}}",type);
+            }
+            query= query.replace("{{type}}",type);
+            return query;
+
     }
 }
 
--- Patika_Klonu/src/patikadev/view/OperatorGUI.java
+++ Patika_Klonu/src/patikadev/view/OperatorGUI.java
@@ -6,7 +6,11 @@ import patikadev.model.Operator;
 import patikadev.model.User;
 
 import javax.swing.*;
+import javax.swing.event.TableModelEvent;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class OperatorGUI extends JFrame {
     private JPanel wrapper;
@@ -24,9 +28,14 @@ public class OperatorGUI extends JFrame {
     private JLabel user;
     private JComboBox cmb_usertype;
     private JTextField fld_name_surname;
-private JLabel field_uname;
+    private JLabel field_uname;
     private JTextField fld_user_id;
     private JButton btn_user_delete;
+    private JTextField fld_sr_username;
+    private JTextField fld_sr_user_uname;
+    private JLabel fld_user_type;
+    private JComboBox cmb_sr_usertype;
+    private JButton btn_user_sh;
     private final Operator operator;
     //bir tabloya dinamik olarak yapıları nasıl aktaracağız dbden çekip:
     private DefaultTableModel mdl_userlist; // bir modele ihtiyacımız var bu sebeple burayı oluşturduk. (yukarıda elle yaptk)
@@ -36,7 +45,7 @@ private JLabel field_uname;
         this.operator = operator;
 
         add(wrapper);
-        setSize(1000, 500);
+        setSize(1200, 600);
         setLocation(Helper.screenCenterPoint("x", getSize()), Helper.screenCenterPoint("y", getSize()));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setTitle(Config.PROJECT_TITLE);
@@ -44,7 +53,15 @@ private JLabel field_uname;
         lbl_welcome.setText("Hoşgeldin : " + operator.getName());
 
         //ModelUserList
-        mdl_userlist = new DefaultTableModel();
+        mdl_userlist = new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                if (column == 0)
+                    return false;
+                return super.isCellEditable(row, column);
+            }
+
+        };
         Object[] col_userlist = {"ID", "Ad Soyad", "Kullanıcı Adı", "Şifre", "Üyelik Tipi"}; // modelimin kolon isimlerini obje veritipinde tanımladım array oluşturdum.
         mdl_userlist.setColumnIdentifiers(col_userlist); // süslü parantez içindekiler tablomdaki başlıklar.
         row_userlist = new Object[col_userlist.length];
@@ -55,6 +72,34 @@ private JLabel field_uname;
         loadUserModel();
         table_userlist.setModel(mdl_userlist);
         table_userlist.getTableHeader().setReorderingAllowed(false);
+        table_userlist.getSelectionModel().addListSelectionListener(e -> {
+            try {
+                String select_user_id = table_userlist.getValueAt(table_userlist.getSelectedRow(), 0).toString();
+                System.out.println(select_user_id);
+                fld_user_id.setText(select_user_id); //seçtiğin satır kullanıcı id kutucuğunda görünecek.
+
+
+            } catch (Exception ex) {
+
+            }
+
+        });
+        table_userlist.getModel().addTableModelListener(e -> {
+            if (e.getType() == TableModelEvent.UPDATE) {
+                int user_id = Integer.parseInt(table_userlist.getValueAt(table_userlist.getSelectedRow(), 0).toString());
+                String user_name = table_userlist.getValueAt(table_userlist.getSelectedRow(), 1).toString();
+                String user_uname = table_userlist.getValueAt(table_userlist.getSelectedRow(), 2).toString();
+                String user_pass = table_userlist.getValueAt(table_userlist.getSelectedRow(), 3).toString();
+                String user_type = table_userlist.getValueAt(table_userlist.getSelectedRow(), 4).toString();
+
+                if (User.update(user_id,user_name,user_uname,user_pass,user_type)){
+                    Helper.showMessage("done");
+
+                }
+                loadUserModel();
+            }
+        });
+
         btn_user_add.addActionListener(e -> {
             if (Helper.isFieldEmpty(fld_name_surname) || Helper.isFieldEmpty(fld_uname) || Helper.isFieldEmpty(fld_password)) {
                 Helper.showMessage("fill");
@@ -78,6 +123,36 @@ private JLabel field_uname;
 
             }
         });
+        btn_user_delete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (Helper.isFieldEmpty(fld_user_id)) {
+                    Helper.showMessage("fill");
+                } else {
+                    int user_id = Integer.parseInt(fld_user_id.getText());
+                    if (User.delete(user_id)) {
+                        Helper.showMessage("done");
+                        loadUserModel();
+                    } else {
+                        Helper.showMessage("error");
+                    }
+                }
+            }
+        });
+        btn_user_sh.addActionListener(e -> {
+            String name = fld_sr_username.getText();
+            String uname = fld_sr_user_uname.getText();
+            String type = cmb_sr_usertype.getSelectedItem().toString();
+            String query = User.searchQuery(name,uname,type);
+
+            ArrayList<User> searchingUser =User.SearchuserList(query);
+            loadUserModel(searchingUser);
+
+        });
+        btn_logout.addActionListener(e -> {
+            dispose();
+
+        });
     }
 
     public void loadUserModel() {
@@ -85,6 +160,20 @@ private JLabel field_uname;
         clearModel.setRowCount(0);
 
         for (User obj : User.getList()) { // veritabanından veri çekip yazdırmanın dinamik yolu.
+            int i = 0;
+            row_userlist[i++] = obj.getId();
+            row_userlist[i++] = obj.getName();
+            row_userlist[i++] = obj.getUname();
+            row_userlist[i++] = obj.getPassword();
+            row_userlist[i++] = obj.getType();
+            mdl_userlist.addRow(row_userlist);
+        }
+    }
+    public void loadUserModel(ArrayList<User> list) {
+        DefaultTableModel clearModel = (DefaultTableModel) table_userlist.getModel(); //eklediğin satır listenin altında gelip eklemedeklerinin gelmemesine yarayan kod.
+        clearModel.setRowCount(0);
+
+        for (User obj : list) { // veritabanından veri çekip yazdırmanın dinamik yolu.
             int i = 0;
             row_userlist[i++] = obj.getId();
             row_userlist[i++] = obj.getName();
